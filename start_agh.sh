11c5a6b17cb3373c6c02a70ee000f0fdf4b30c04
siritami <103488094+siritami@users.noreply.github.com>
2024-05-26 01:48:41 +0700

    Update start_agh.sh

--- start_agh.sh
+++ start_agh.sh
@@ -4,7 +4,7 @@ if [ ! -e /opt/AdGuardHome/config/AdGuardHome.yaml ]; then
   cp /AdGuardHome.yaml /opt/AdGuardHome/config/AdGuardHome.yaml
 fi
 
-chmod 600 /opt/AdGuardHome/config/AdGuardHome.yaml
-chmod 600 /config/AdGuardHome.yaml
+chmod 666 /opt/AdGuardHome/config/AdGuardHome.yaml
+chmod 666 /config/AdGuardHome.yaml
 
 /opt/AdGuardHome/AdGuardHome -c /config/AdGuardHome.yaml
