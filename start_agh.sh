b744c1c1f5a7550d6c2e47bea5dbe746b8ee6c06
siritami <103488094+siritami@users.noreply.github.com>
2024-05-26 02:41:20 +0700

    Update start_agh.sh

--- start_agh.sh
+++ start_agh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chmod 666 /opt/adguardhome/conf/AdGuardHome.yaml
-/opt/AdGuardHome/AdGuardHome -c /opt/adguardhome/conf/AdGuardHome.yaml
+chmod 666 /opt/AdGuardHome/config/AdGuardHome.yaml
+/opt/AdGuardHome/AdGuardHome -c /opt/AdGuardHome/config/AdGuardHome.yaml
